dfe808510768286aa4b36496a8f3555ff19dc275
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-04-30 20:28:44 -0300

    :tada: feat: created model User

---
+++ src/main/java/io/github/karMiguel/library/model/Permission.java
@@ -0,0 +1,45 @@
+package io.github.karMiguel.library.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "permission")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Permission that = (Permission) o;
+        return Objects.equals(id, that.id) && Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
