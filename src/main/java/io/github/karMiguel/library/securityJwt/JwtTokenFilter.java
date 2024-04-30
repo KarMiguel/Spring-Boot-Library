857807da20447d10941f26af1fe2509f114add0f
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-04-30 20:27:36 -0300

    :tada: feat: config jwt e spring security

---
+++ src/main/java/io/github/karMiguel/library/securityJwt/JwtTokenFilter.java
@@ -0,0 +1,37 @@
+package io.github.karMiguel.library.securityJwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtTokenFilter extends GenericFilterBean {
+
+
+    private JwtTokenProvider tokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String token = tokenProvider.resolveToken((HttpServletRequest) request);
+        if (token != null && tokenProvider.validateToken(token)) {
+            Authentication auth = tokenProvider.getAuthentication(token);
+            if (auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
