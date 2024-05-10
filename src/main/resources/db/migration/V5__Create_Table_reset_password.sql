6dbbc2da3b08e1aa615ae7bbbb5b838a6ac8572f
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-05-09 21:12:47 -0300

    :bricks: ci: created table reset_password

---
+++ src/main/resources/db/migration/V5__Create_Table_reset_password.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE reset_password (
+    id SERIAL PRIMARY KEY,
+    id_cliente BIGINT REFERENCES users(id),
+    status VARCHAR(25),
+    code VARCHAR(25),
+    new_password VARCHAR(255),
+    date_created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
