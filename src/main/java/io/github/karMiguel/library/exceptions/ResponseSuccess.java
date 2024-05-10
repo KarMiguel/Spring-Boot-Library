b930141c4378bf729b61e8af83aae97ba318b404
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-05-09 21:16:07 -0300

    :sparkles: feat: exception success response

---
+++ src/main/java/io/github/karMiguel/library/exceptions/ResponseSuccess.java
@@ -0,0 +1,16 @@
+package io.github.karMiguel.library.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseSuccess {
+
+    public  String message;
+
+}
